6981fd35e14a339aa92140883a5f7ced20cba87b
Julia Li <jull@microsoft.com>
2019-09-12 03:41:20 +0800

    Add a new file

---
+++ e_Recursion.py
@@ -0,0 +1,22 @@
+def recursion(n):
+
+    if n <= 0:
+        exit(0)
+    else:
+        print(n)
+        recursion(n - 1)
+
+    return
+
+def recursion_2(n):
+
+    ### TODO Add Logic
+    ### 修改recursion()的代码，使得recursion_2(3)的打印结果为1\n 2\n 3
+
+    ###
+
+    return
+
+if __name__ == "__main__":
+
+    recursion(3)
